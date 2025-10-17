e5917b0e946c2089e86d074f170fab7ad606054d
ThatGuyIain <iklotzba@ucsc.edu>
2025-10-17 01:12:23 +0000

    Allowed user to draw on canvas (Step 2 Complete) - Added clear button - Added event listeners that check when the mouse is down and if it is moving on the canvas to draw
    Co-authored-by: JaydenRRamirez <JaydenRRamirez@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,39 @@ document.body.innerHTML = `
 
 const canvas = document.createElement("canvas");
 canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
 document.body.appendChild(canvas);
+
+const cursor = { active: false, x: 0, y: 0 };
+
+const ctx = canvas.getContext("2d")!;
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx.beginPath();
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+document.body.appendChild(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
